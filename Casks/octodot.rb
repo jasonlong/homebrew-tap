dea5574e2583fa272fc722e51ea005ba439c70ca
Jason Long <jason@jasonlong.me>
2026-04-08 08:54:51 -0400

    Add octodot cask v0.4.1

---
+++ Casks/octodot.rb
@@ -0,0 +1,23 @@
+cask "octodot" do
+  version "0.4.1"
+  sha256 "b07752f81a4f4dda7f281e0e4367011984156a35c7435fa32f29285afdfcbc0f"
+
+  url "https://github.com/jasonlong/octodot/releases/download/v#{version}/Octodot-v#{version}-macos.zip"
+  name "Octodot"
+  desc "GitHub notifications in the macOS menu bar"
+  homepage "https://github.com/jasonlong/octodot"
+
+  depends_on macos: ">= :ventura"
+
+  preflight do
+    system_command "/usr/bin/xattr",
+      args: ["-cr", "#{staged_path}/Octodot.app"]
+  end
+
+  app "Octodot.app"
+
+  zap trash: [
+    "~/Library/Application Support/Octodot",
+    "~/Library/Preferences/com.octodot.app.plist",
+  ]
+end
