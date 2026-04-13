b29fc41bfe6f0dca78ee292776e09851cb1932bf
Jason Long <jason@jasonlong.me>
2026-04-13 13:28:45 -0400

    Add mater cask (placeholder)

---
+++ Casks/mater.rb
@@ -0,0 +1,17 @@
+cask "mater" do
+  version "3.0.0-beta.2"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/jasonlong/mater/releases/download/v#{version}/Mater-v#{version}-macos.zip"
+  name "Mater"
+  desc "Pomodoro timer in the macOS menu bar"
+  homepage "https://github.com/jasonlong/mater"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Mater.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.jasonlong.mater.plist",
+  ]
+end
