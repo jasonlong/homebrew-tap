479f77835f0568fcdab1fd7eaf53d56b92643422
Jason Long <jason@jasonlong.me>
2026-08-24 10:03:17 -0400

    Update octodot to 0.5.10

--- Casks/octodot.rb
+++ Casks/octodot.rb
@@ -1,6 +1,6 @@
 cask "octodot" do
-  version "0.5.8"
-  sha256 "860c28a05edf6d360c2767743b3843cc163c02364efc96015ad4adb09ffed370"
+  version "0.5.10"
+  sha256 "62c39c3ced84dd97e29e8a351bf380c627774bf1844042f67e0437d7f5ff0925"
 
   url "https://github.com/jasonlong/octodot/releases/download/v#{version}/Octodot-v#{version}-macos.zip"
   name "Octodot"
